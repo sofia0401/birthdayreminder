196d06ffb25b7f5b296c27f307aa5174e2d77d85
sofia0401 <sofia0401@yandex.ru>
2021-10-04 16:02:12 +0300

    Добавила пользовательское меню.

--- src/main/java/com/example/birthdayreminder/BirthdayReminderApplication.java
+++ src/main/java/com/example/birthdayreminder/BirthdayReminderApplication.java
@@ -4,15 +4,45 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class BirthdayReminderApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         Scanner sc=new Scanner(System.in);
         File file = new File("src/main/resources/calendar.txt");
         birthdayCalendar cl=new birthdayCalendar(file);
-        cl.getAll();
+        System.out.println("1 - узнать, чей сегодня день рождения\n" +
+                "2 - добавить друга и его дату рождения\n" +
+                "3 - показать всех друзей и их дни рождения\n" +
+                "Введите номер команды:");
+        int cmd=sc.nextInt();
+        switch (cmd) {
+            case 1: {
+                if (cl.checkToday()==null)
+                    System.out.println("Сегодня обычный день :(");
+                else
+                    System.out.println("Сегодня " + cl.checkToday() + " отмечает день рождения! Не забудьте поздравить");
+                break;
+            }
+            case 2: {
+                System.out.println("Введите данные в формате 'Имя ДД-ММ-ГГГГ':");
+                sc.nextLine();
+                String newRecord =sc.nextLine();
+                if (cl.writing(newRecord))
+                    System.out.println("Данные успешно добавлены");
+                else
+                    System.out.println("Что-то пошло не так");
+                break;
+            }
+            case 3: {
+                if (!(cl.getAll()))
+                    System.out.println("Вы не сохранили ни одной даты");
+                break;
+            }
+            default: break;
+        }
     }
 
 }
